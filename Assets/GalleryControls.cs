9f57b7c3d71b3589d97ecd24fe7471bfc846fcac
Thomas Day <Tday42@gmail.com>
2016-01-16 14:12:42 +0000

    Initial Commit

---
+++ Assets/GalleryControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GalleryControls : MonoBehaviour {
+
+	private GameObject[] objects;
+    private Ray ray;
+	private RaycastHit[] hits;
+
+	// Use this for initialization
+	void Start () {
+        objects = GameObject.FindGameObjectsWithTag("target");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ray = new Ray(gameObject.transform.position, gameObject.transform.forward);
+        //Debug.DrawRay(this.transform.position, this.transform.forward, Color.green);
+        hits = Physics.RaycastAll(ray);
+
+        foreach (GameObject gobj in objects)
+        {
+            Behaviour b = (Behaviour)gobj.GetComponent("Halo");
+            b.enabled = false;
+        }
+
+		foreach (RaycastHit hit in hits){
+            bool state = false;
+            if (hit.collider.tag == "target")
+            {
+                state = true;
+            }
+
+            Behaviour bt = (Behaviour)hit.collider.GetComponent("Halo");
+            bt.enabled = state;
+        }
+	}
+}
