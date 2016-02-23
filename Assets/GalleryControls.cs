48728a526c2e5c0d1f9fa058c6cd64ba1e6c1e56
Thomas Day <t.day@chester.ac.uk>
2016-02-23 10:47:47 +0000

    Updated Animation Controls

--- Assets/GalleryControls.cs
+++ Assets/GalleryControls.cs
@@ -6,12 +6,12 @@ public class GalleryControls : MonoBehaviour {
 	private GameObject[] objects;
 	private GameObject cObj;
 	public GameObject main;
-	public Animator cursorAnim;
+	public Animator CursorAnim;
+
+	private bool play = false;
 
     private Ray ray;
 	private RaycastHit[] hits;
-
-	//private CardboardHead head;
 
 	private float time = 0, speed = 1;
 
@@ -20,13 +20,13 @@ public class GalleryControls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         objects = GameObject.FindGameObjectsWithTag("target");
-	//	head = this.GetComponent<CardboardHead> ();
+
+		CursorAnim.Play ("Default");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ray = new Ray(gameObject.transform.position, gameObject.transform.forward);
-        //Debug.DrawRay(this.transform.position, this.transform.forward, Color.green);
         hits = Physics.RaycastAll(ray);
 
         foreach (GameObject gobj in objects)
@@ -36,15 +36,13 @@ public class GalleryControls : MonoBehaviour {
         }
 
 		foreach (RaycastHit hit in hits){
-            bool state = false;
             if (hit.collider.tag == "target")
             {
 				Debug.Log ("Hit Target");
-                state = true;
 				cObj = hit.collider.gameObject;
 
 				Behaviour bt = (Behaviour)cObj.GetComponent("Halo");
-				bt.enabled = state;
+				bt.enabled = true;
             }
         }
 
@@ -61,8 +59,8 @@ public class GalleryControls : MonoBehaviour {
 			Debug.Log ("Moving");
 			float step = speed * Time.deltaTime;
 
-			Debug.Log (main.transform.position);
-			Debug.Log (cObj.transform.position);
+			//Debug.Log (main.transform.position);
+			//Debug.Log (cObj.transform.position);
 
 			Vector3 temp = Vector3.MoveTowards(main.transform.position, cObj.transform.position, step);
 
@@ -74,15 +72,17 @@ public class GalleryControls : MonoBehaviour {
 	}
 
 	public void startTimer () {
-		Debug.Log ("Timer Started");
-		//Start Animation
-		//cursorAnim.StartPlayback();
+		//Debug.Log ("Timer Started");
+
+		CursorAnim.Play ("Circle");
+
 		timing = true;
 	}
 	public void stopTimer() {
-		Debug.Log ("Timer Stopped");
-		//Reset Animation
-		//cursorAnim.StopPlayback();
+		//Debug.Log ("Timer Stopped");
+
+		CursorAnim.Play ("Default");
+
 		timing = false;
 		time = 0;
 	}
