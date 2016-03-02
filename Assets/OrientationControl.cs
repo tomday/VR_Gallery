8f212390d4be0886941117825e00e7e49f518aab
Thomas Day <t.day@chester.ac.uk>
2016-03-02 16:35:16 +0000

    Updated Orientation Controls

--- Assets/OrientationControl.cs
+++ Assets/OrientationControl.cs
@@ -4,60 +4,40 @@ using System.Collections;
 
 public class OrientationControl : MonoBehaviour {
 
-	private string level;
+	private string level = "Menu";
 
 	private bool loaded = false;
 
-	private Ray ray;
-	private RaycastHit hit;
-
-	public GameObject levelsHolder;
+	private bool isLandscape = false, isPortrait = true;
 
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad (this);
-	
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frameDo you still have that double sided sticky taop
 	void Update () {
-		Debug.DrawRay (this.transform.position, this.transform.forward);
-		ray = new Ray (this.transform.position, this.transform.forward);
 
-		if (Input.GetMouseButtonDown (0)) {
-			if (Physics.Raycast (ray, out hit, 100)) {
-				Debug.Log(hit.collider.name); 
-				level = hit.collider.name;
-			}
+		isLandscape = (Input.deviceOrientation == DeviceOrientation.LandscapeLeft);
+		if (isLandscape)
+			Debug.Log ("Landscape");
+		isPortrait = (Input.deviceOrientation == DeviceOrientation.Portrait);
+		if (isPortrait)
+			Debug.Log ("Portrait");
+
+		//Debug.Log (SceneManager.GetActiveScene ().name);
+
+		if (isLandscape && SceneManager.GetActiveScene().name == "Menu") {
+			SceneManager.LoadSceneAsync (level);
 		}
-
-		if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch (0);
-			if (touch.phase == TouchPhase.Moved) {
-				Debug.Log (touch.deltaPosition);
-				levelsHolder.transform.RotateAround (new Vector3(0, 0, 0), new Vector3(0, 1, 0), (float)(touch.deltaPosition.x * Time.deltaTime));
-
-			}
-
-			if (touch.phase == TouchPhase.Began) {
-				if (Physics.Raycast (ray, out hit, 100)) {
-					level = hit.collider.name;
-				}
-			}
-		} 
-
-		if (SceneManager.GetActiveScene ().name != level)
-			loaded = false;
-
-		if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight) {
-			if (!loaded) {
-				SceneManager.LoadScene (level);
-				loaded = true;
-			}
+		if (isPortrait && SceneManager.GetActiveScene().name != "Menu") {
+			SceneManager.LoadSceneAsync ("Menu");
+			level = "Menu";
 		}
+	}
 
-		if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) {
-				SceneManager.LoadScene ("menu");
-		}
+	public void setLevel(string name){
+		level = name;
+		Debug.Log("Selected Level: " + level);
 	}
 }
