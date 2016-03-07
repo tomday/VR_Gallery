70ea519dca0084a61b03ce58ba2845eb63bc381b
Thomas Day <t.day@chester.ac.uk>
2016-03-07 16:29:25 +0000

    Updated Menu UIO

--- Assets/OrientationControl.cs
+++ Assets/OrientationControl.cs
@@ -6,7 +6,7 @@ public class OrientationControl : MonoBehaviour {
 
 	private string level = "Menu";
 
-	private bool loaded = false;
+	//private bool loaded = false;
 
 	private bool isLandscape = false, isPortrait = true;
 
@@ -15,24 +15,26 @@ public class OrientationControl : MonoBehaviour {
 		DontDestroyOnLoad (this);
 	}
 	
-	// Update is called once per frameDo you still have that double sided sticky taop
+	// Update is called once per frame
 	void Update () {
 
-		isLandscape = (Input.deviceOrientation == DeviceOrientation.LandscapeLeft);
-		if (isLandscape)
-			Debug.Log ("Landscape");
+		isLandscape = (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight);
+		//if (isLandscape)
+		//	Debug.Log ("Landscape");
 		isPortrait = (Input.deviceOrientation == DeviceOrientation.Portrait);
-		if (isPortrait)
-			Debug.Log ("Portrait");
+		//if (isPortrait)
+		//	Debug.Log ("Portrait");
 
 		//Debug.Log (SceneManager.GetActiveScene ().name);
 
 		if (isLandscape && SceneManager.GetActiveScene().name == "Menu") {
 			SceneManager.LoadSceneAsync (level);
+			Debug.Log (level + " loaded");
 		}
 		if (isPortrait && SceneManager.GetActiveScene().name != "Menu") {
 			SceneManager.LoadSceneAsync ("Menu");
 			level = "Menu";
+			Debug.Log (level + "loaded");
 		}
 	}
 
